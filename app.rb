b2698e6aca58845b6352004fa785f3e948957d05
Karen V <karenvo@usc.edu>
2019-11-05 06:07:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -26,8 +26,8 @@ class App < Sinatra::Base
     # gets a 200 status Code
     # displays the product of the two numbers in the route
     # is not hard-coded
-    @num1 = params[:num1]
-    @num2 = params[:num2]
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
     "#{@num1 * @num2}"
   end
 end
